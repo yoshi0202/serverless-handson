8ffc902d4e665e61d28a97b01849f5fdddcaaf61
yoshi0202 <kobayashi020202@gmail.com>
2020-11-26 00:07:27 +0900

    feat: use main methods

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const IndexController = require("./controller/IndexController");
+const HelloController = require("./controller/HelloController");
 exports.handler = async (event) => {
   switch (event.rawPath) {
     case "/":
       return await IndexController.main();
 
     case "/hello":
-      console.log("hello!!!");
-      break;
+      return HelloController.main();
 
     default:
       break;
