b4babb9f13324f9365eeb7c54208c2cd9d6d24b0
yoshi0202 <kobayashi020202@gmail.com>
2020-11-27 00:11:22 +0900

    feat: create getNewPost methods

--- services/QiitaService.js
+++ services/QiitaService.js
@@ -1,4 +1,5 @@
 const QiitaDTO = require("../QiitaDTO");
+const RequestModule = require("../RequestModule");
 module.exports = class QiitaService {
   getTitle(data) {
     return data.title;
@@ -16,5 +17,15 @@ module.exports = class QiitaService {
     return apiResArray.map((apiRes) => {
       return this.createQiitaDTO(apiRes);
     });
+  }
+
+  async getNewPost() {
+    const request = new RequestModule(
+      "qiita.com",
+      "/api/v2/items?page=1&per_page=10",
+      443,
+      "GET"
+    );
+    return await request.send();
   }
 };
