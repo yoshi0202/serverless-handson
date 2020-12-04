a2f0c8c7fe8d2a0e87805183ff4b52d91f4bda44
yoshi0202 <kobayashi020202@gmail.com>
2020-12-04 19:05:23 +0900

    feat: create /hello

--- controller/HelloController.js
+++ controller/HelloController.js
@@ -1,5 +1,31 @@
+const HTTPRequest = require("../HTTPRequest");
 module.exports = class HelloController {
-  static main() {
-    return "Hello!";
+  static async main(requestBody) {
+    if (!process.env.LINE_TOKEN) {
+      console.error(
+        `LINE_TOKENが設定されていません LINE_TOKEN=${process.env.LINE_TOKEN}`
+      );
+      return "";
+    }
+    const request = new HTTPRequest();
+    try {
+      let body = JSON.parse(requestBody);
+      await request.post(
+        "https://api.line.me/v2/bot/message/push",
+        {
+          to: body.events[0].source.userId,
+          messages: [{ type: "text", text: a.events[0].message.text }],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.LINE_TOKEN}`,
+          },
+        }
+      );
+      return "OK";
+    } catch (error) {
+      return `Err: ${error}`;
+    }
   }
 };
